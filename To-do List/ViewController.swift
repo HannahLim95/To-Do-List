1454b8c7ffbfdf92c1035538258505350aed6cc2
Hannah Lim <hannahlim@MacBook-Pro-van-Hannah.local>
2015-09-26 11:36:00 +0200

    Final commit

--- To-do List/ViewController.swift
+++ To-do List/ViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var textNewTodo: UITextField!
     @IBOutlet weak var labelNewTodo: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
-    let toDoList = NSUserDefaults.standardUserDefaults()
+    let defaults = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,16 +28,55 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonAddTodo(sender: AnyObject) {
-        labelNewTodo.text = textNewTodo.text
         if(textNewTodo.text != ""){
-            var listToDo = toDoList.arrayForKey("todolist")
             var newToDo = textNewTodo.text
-            listToDo!.append(newToDo)
-            toDoList.setObject(listToDo!, forKey: "todolist")
-            print(toDoList.arrayForKey("todolist")!)
+            if (defaults.arrayForKey("todolist") == nil) {
+                var listArray = [newToDo]
+                defaults.setObject(listArray, forKey: "todolist")
+            } else {
+                var listToDo = defaults.arrayForKey("todolist")
+                listToDo!.append(newToDo)
+                defaults.setObject(listToDo!, forKey: "todolist")
+            }
+            print(defaults.arrayForKey("todolist")!)
+            tableView.reloadData()
         }
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return defaults.arrayForKey("todolist")!.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell
+        cell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        var listToDo = defaults.arrayForKey("todolist")!
+        cell.textLabel?.text = listToDo[indexPath.row] as? String
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    //Alleen maar gelukt om een korte click te doen, maar hij doet het eindelijk wel!
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!){
+        var listToDo = defaults.arrayForKey("todolist")!
+        listToDo.removeAtIndex(indexPath.row)
+        defaults.setObject(listToDo, forKey: "todolist")
+        tableView.reloadData()
+    }
+    
+    
+
+
 
 }
 
- 
+
+
+
+
+
